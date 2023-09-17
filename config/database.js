cf9d5f7eb6c4ec8becbd04eff4dbfc32dd3fb4a7
archimed2023 <142899604+archimed2023@users.noreply.github.com>
2023-09-17 03:48:58 +0300

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -158,8 +158,7 @@ module.exports = ({ env }) => {
     connection: {
       client,
       ...connections[client],
-      // acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
-      acquireConnectionTimeout: 5000, pool: { min: 0, max: 10, createTimeoutMillis: 8000, acquireTimeoutMillis: 8000, idleTimeoutMillis: 8000, reapIntervalMillis: 1000, createRetryIntervalMillis: 100, },
+      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
     },
   };
 };
